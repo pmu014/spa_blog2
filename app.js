fb561544e547aa3a96aa79d162ed61c2acb7e41e
Minuk <ansems@naver.com>
2022-12-30 04:42:27 +0900

    회원가입, 로그인 성공

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-asdasdads
+const express = require("express");
+const authRouter = require("./routes/auth");
+// const postsRouter = require("./routes/board");
+// const commentRouter = require("./routes/comment");
+const app = express();
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+
+app.use(express.json());
+app.use("/", express.urlencoded({extended: false}), [authRouter,
+    // postsRouter,commentsRouter,likesRouter
+])
+
+
+
+app.listen(8080, () => {
+  console.log("서버가 요청을 받을 준비가 됬어요");
+});
